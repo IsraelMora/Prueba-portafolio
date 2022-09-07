6ab790a8f717c325b2a8ad929fc1c28580c0abb9
Israel Mora <israel_mora@banvenez.com>
2022-09-07 15:40:45 -0400

    first commit

---
+++ src/routes/layout.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import Navbar from "../components/Navbar.svelte";
+    import { Router, Route } from "svelte-routing";
+    import Inicio from "./pages/inicio.svelte";
+    import Segundo from "./pages/segundo.svelte";
+    import Tercero from "./pages/tercero.svelte";
+    import Footer from "../components/Footer.svelte";
+
+    let menu = [
+        {
+            name: "Inicio",
+            rut: "/",
+        },
+        {
+            name: "Primero",
+            rut: "/primero",
+        },
+        {
+            name: "Segundo",
+            rut: "/segundo",
+        },
+    ];
+</script>
+
+<div class="contenerdor text-center">
+    <div class="dark:bg-slate-800 bg-blue-600 h-screen ">
+        <div>
+            <Navbar navbar={menu} />
+        </div>
+        <br /><br /><br />
+        <div class="bg-white w-11/12 mx-auto mb-8 mt-7 h-3/4">
+            <Router>
+                <Route path="/">
+                    <Inicio />
+                </Route>
+                <Route path="/primero">
+                    <Segundo />
+                </Route>
+                <Route path="/segundo">
+                    <Tercero />
+                </Route>
+            </Router>
+        </div>
+        <div class="">
+            <Footer />
+        </div>
+    </div>
+</div>
