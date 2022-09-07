cfaa4e1170bd44156fcc8c4942473e435e40a640
Israel Mora <israel_mora@banvenez.com>
2022-09-07 17:30:48 -0400

    cambio ruta de despliegue

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# if you are deploying to https://IsraelMora.github.io/Prueba-portafolio
-# git push -f git@github.com:IsraelMora/Prueba-portafolio.git main:gh-pages
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:israelmora/Prueba-portafolio.git main:gh-pages
 
 cd -
